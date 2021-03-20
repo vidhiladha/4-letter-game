47db64e221134aab0e0448ffacc391601d1e47cf
vidhiladha <74768975+vidhiladha@users.noreply.github.com>
2021-03-20 14:52:41 +0530

    Add files via upload

--- Game.js
+++ Game.js
@@ -6,5 +6,23 @@ class Game {
     gameStateRef.on("value",function(data){
        gameState = data.val();
     })
+
+    
+    
+
 }  
+
+    async start() {
+        if(gameState === 0){
+            player = new Player
+            var playerCountRef = await database.ref('playerCount').once("value");
+            if(playerCountRef.exists()){
+            playerCount = playerCountRef.val();
+            player.getCount();
+            }
+        }
+
+    form = new form()
+    form.display();
+    }
 }
